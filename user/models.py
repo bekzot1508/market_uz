3fabd0ebf991b8271ceb8f74c5e540d0ac48c025
bezkot1508 <daminovbekzod13@gmail.com>
2025-11-09 20:13:36 +0500

    set up is done, with security

---
+++ user/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(default='default_profile_pic.jpg')
+    city = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    street  = models.CharField(max_length=100)
+    house = models.CharField(max_length=100)
